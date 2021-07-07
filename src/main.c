093be561250b72ade72a7d16bf7aae9d2af830a1
Craig Prevallet <penguintx@hotmail.com>
2021-07-06 21:58:02 -0500

    Get planets to generate output.

--- src/main.c
+++ src/main.c
@@ -1,9 +1,5 @@
 #include <gtk/gtk.h>
 
-typedef struct {
-    GtkTextBuffer *textbuffer1;
-} app_widgets;
-
 // globals
 GtkTextBuffer *textbuffer1;
 GtkSpinButton *sb_hr;
@@ -11,64 +7,85 @@ GtkSpinButton *sb_min;
 GtkSpinButton *sb_sec;
 GtkSpinButton *sb_int;
 GtkSpinButton *sb_numint;
-/*
-int get_values() {
-    gdouble hr = gtk_spin_button_get_value (sb_hr);
-	//sprintf(tmp, "spin=%d", (int) );
-    printf("hr=%d\n", (int)hr);
-    return 0;
-}
-*/
-void _on_clicked(GtkButton *b, app_widgets *w) {
-  printf("clicked.\n" );
+GtkCalendar   *cal;
+GtkComboBox   *cb_planet;
 
-  //get_values();
+int get_values() {
+    int hr = gtk_spin_button_get_value_as_int(sb_hr);
+    int min = gtk_spin_button_get_value_as_int(sb_min);
+    int sec = gtk_spin_button_get_value_as_int(sb_sec);
+    float interval = gtk_spin_button_get_value(sb_int);
+    int num_intervals = gtk_spin_button_get_value_as_int(sb_numint);
+    guint year;
+    guint mon;
+    guint day;
+    gtk_calendar_get_date(cal, &year, &mon, &day);
+    gint active_item = gtk_combo_box_get_active(cb_planet);
+    FILE *fp;
+    fp = fopen("/tmp/aa.txt", "w+");
+    if(fp) {
+        fprintf(fp, "%d\n", year);
+        fprintf(fp, "%d\n", mon+1);
+        fprintf(fp, "%d\n", day);
+        fprintf(fp, "%d\n", hr);
+        fprintf(fp, "%d\n", min);
+        fprintf(fp, "%d\n", sec);
+        fprintf(fp, "%f\n", interval);
+        fprintf(fp, "%d\n", num_intervals);
+        fprintf(fp, "%d\n", active_item);
+    }
+    fclose(fp);
+        return 0;
+}
+
+void _on_clicked(GtkButton *b) {
+  get_values();
   GtkTextMark *mark;
   GtkTextIter iter;
   FILE *fp;
-  char path[1035];
+  char line[1035]; //why this big???
 
-  /* Open the command for reading. */
-  fp = popen("/usr/bin/aa < /home/craig/rust/astronomical-almanac/aa.que", "r");
+  // Open the command for reading.
+  fp = popen("/usr/bin/aa < /tmp/aa.txt", "r");
 
   if (fp == NULL) {
     printf("Failed to run command\n" );
+    printf("Astronomical almanac must be installed for this program to function.\n" );
     exit(1);
   }
 
-  /* Read the output a line at a time - output it. */
-  while (fgets(path, sizeof(path), fp) != NULL) {
-    mark = gtk_text_buffer_get_insert (w->textbuffer1);
-    gtk_text_buffer_get_iter_at_mark (w->textbuffer1, &iter, mark);
-    gtk_text_buffer_insert (w->textbuffer1, &iter, path, -1);
+  // Read the output a line at a time - and display it.
+  while (fgets(line, sizeof(line), fp) != NULL) {
+    mark = gtk_text_buffer_get_insert (textbuffer1);
+    gtk_text_buffer_get_iter_at_mark (textbuffer1, &iter, mark);
+    gtk_text_buffer_insert (textbuffer1, &iter, line, -1);
   }
 
-  /* close */
+  // close
   pclose(fp);
-}
-
-void hours_changed() {
 }
 
 int main(int argc, char *argv[])
 {
     GtkBuilder      *builder; 
     GtkWidget       *window;
-    app_widgets     *widgets = g_slice_new(app_widgets);
 
     gtk_init(&argc, &argv);
 
     builder = gtk_builder_new_from_file("glade/example.glade");
 
     window = GTK_WIDGET(gtk_builder_get_object(builder, "window1"));
-    widgets->textbuffer1  = GTK_TEXT_BUFFER(gtk_builder_get_object(builder, "textbuffer1"));
-    sb_hr  = GTK_SPIN_BUTTON(gtk_builder_get_object(builder, "sb_hr"));
-    sb_min  = GTK_SPIN_BUTTON(gtk_builder_get_object(builder, "sb_min"));
-    sb_sec  = GTK_SPIN_BUTTON(gtk_builder_get_object(builder, "sb_sec"));
-    sb_int  = GTK_SPIN_BUTTON(gtk_builder_get_object(builder, "sb_int"));
-    sb_numint  = GTK_SPIN_BUTTON(gtk_builder_get_object(builder, "sb_numint"));
+    textbuffer1  = GTK_TEXT_BUFFER(gtk_builder_get_object(builder, "textbuffer1"));
+    sb_hr  = GTK_SPIN_BUTTON(gtk_builder_get_object(builder, "sb1"));
+    sb_min  = GTK_SPIN_BUTTON(gtk_builder_get_object(builder, "sb2"));
+    sb_sec  = GTK_SPIN_BUTTON(gtk_builder_get_object(builder, "sb3"));
+    sb_int  = GTK_SPIN_BUTTON(gtk_builder_get_object(builder, "sb4"));
+    sb_numint  = GTK_SPIN_BUTTON(gtk_builder_get_object(builder, "sb5"));
+    cal = GTK_CALENDAR(gtk_builder_get_object(builder, "cal")); 
+    cb_planet = GTK_COMBO_BOX(gtk_builder_get_object(builder, "cb_planet1"));
 
-    gtk_builder_connect_signals(builder, widgets);
+    // gtk_builder_connect_signals(builder, widgets);
+    gtk_builder_connect_signals(builder, NULL);
 
 
     g_object_unref(builder);
@@ -76,8 +93,6 @@ int main(int argc, char *argv[])
     gtk_widget_show(window);                
     gtk_main();
     
-    g_slice_free(app_widgets, widgets);
-
     return 0;
 }
 
